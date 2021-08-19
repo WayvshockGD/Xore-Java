c768f814839c1de7b1bbe8444f7e74dccca1a1dd
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2021-08-19 01:22:12 -0400

    Delete CommandCtx.java

--- src/main/java/com/wayv/xore/interfaces/CommandCtx.java
+++
@@ -1,9 +0,0 @@
-package com.wayv.xore.interfaces;
-
-import discord4j.core.GatewayDiscordClient;
-import discord4j.core.object.entity.Message;
-
-public interface CommandCtx {
-    GatewayDiscordClient getClient();
-    Message getMessage();
-}
